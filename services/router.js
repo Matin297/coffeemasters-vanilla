d0df1b8e9fd6c2c089848ed721be71e695e06b48
Matin Mir <asmamir297@gmail.com>
2023-11-16 16:05:58 +0330

    implement app router init method

--- services/router.js
+++ services/router.js
@@ -1,5 +1,18 @@
 const router = {
-  init() {},
+  init() {
+    const anchorLinks = document.querySelectorAll("a.navlink");
+
+    anchorLinks.forEach((anchorLink) => {
+      anchorLink.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const href = event.target.getAttribute("href");
+        this.go(href);
+      });
+    });
+
+    this.go(location.pathname);
+  },
   go(route, addToHistory = true) {},
 };
 
