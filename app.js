37c6b994e32a4d6b3811464ec61b424898472d7e
Matin Mir <asmamir297@gmail.com>
2023-11-16 14:39:24 +0330

    made store globally available through window

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import store from "./services/store";
+
+window.coffee_app = {};
+coffee_app.store = store;
+
 window.addEventListener("DOMContentLoaded", () => {
   console.log("dom is ready");
 });
