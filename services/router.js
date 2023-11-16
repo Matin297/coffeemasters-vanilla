f923f3c1a37fcd3022a8dfe23e15a13060698ee3
Matin Mir <asmamir297@gmail.com>
2023-11-16 16:32:29 +0330

    implement app router go method

--- services/router.js
+++ services/router.js
@@ -13,7 +13,35 @@ const router = {
 
     this.go(location.pathname);
   },
-  go(route, addToHistory = true) {},
+  go(route, addToHistory = true) {
+    console.log(`going to ${route}`);
+
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Home";
+        break;
+
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Orders";
+        break;
+    }
+
+    if (pageElement) {
+      const mainWrapper = document.querySelector("main");
+      mainWrapper.innerHTML = "";
+      mainWrapper.append(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
 };
 
 export default router;
