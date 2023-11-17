81c2d9e796ac8286cc5b3007e2ae41413d22e0af
Matin Mir <asmamir297@gmail.com>
2023-11-17 11:58:54 +0330

    set up details/order/menu page web components

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import router from "./services/router.js";
 import store from "./services/store.js";
 import { loadData } from "./services/menu.js";
 
+import { DetailsPage } from "./components/DetailsPage.js";
+import { MenuPage } from "./components/MenuPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+
 window.coffee_app = {};
 coffee_app.store = store;
 coffee_app.router = router;
