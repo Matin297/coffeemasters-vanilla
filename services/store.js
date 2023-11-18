95eceb9692b3707d531cd2e4936096b32dd17d99
Matin Mir <asmamir297@gmail.com>
2023-11-18 11:14:40 +0330

    set up store proxy and broadcast custum events

--- services/store.js
+++ services/store.js
@@ -3,4 +3,20 @@ const store = {
   cart: [],
 };
 
-export default store;
+const storeProxy = new Proxy(store, {
+  set(target, property, value) {
+    target[property] = value;
+
+    if (property === "menu") {
+      window.dispatchEvent(new Event("menuchange"));
+    }
+
+    if (property === "cart") {
+      window.dispatchEvent(new Event("cartchange"));
+    }
+
+    return true;
+  },
+});
+
+export default storeProxy;
