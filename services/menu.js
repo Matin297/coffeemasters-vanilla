f20b26986fb1e898c0bd4af9e5e1bd611dca49b8
Matin Mir <asmamir297@gmail.com>
2023-11-19 15:58:58 +0330

    add getproductbyid function

--- services/menu.js
+++ services/menu.js
@@ -4,3 +4,23 @@ export async function loadData() {
   const menuData = await API.fetchMenu();
   coffee_app.store.menu = menuData;
 }
+
+export async function getProductById(category_name, product_id) {
+  if (coffee_app.store.menu === null) {
+    await loadData();
+  }
+
+  const category = coffee_app.store.menu.find(
+    (category) => category.name === category_name
+  );
+
+  if (!category) return null;
+
+  const product = category.products.find(
+    (product) => product.id === product_id
+  );
+
+  if (!product) return null;
+
+  return product;
+}
